d5a1203ed1573611ac6eb49c479f351df5945461
dacong <2540740154@qq.com>
2017-12-07 22:13:23 +0800

    重写S3

--- S3/index.js
+++ S3/index.js
@@ -1,61 +1,47 @@
 window.onload = function () {
-  var req = [];
-  Init(req);
-
-  $("#button").mouseleave(function () {
-    clear(req);
-    Init(req);
-  });
-
-  $("#button").mouseenter(function () {
-    $("li").click();
-  });
+  $("#button").mouseleave(clear);
+  $("#button").mouseenter(Init);
 }
 
-function Init(req) {
-  $("li").click(function() {
-    if($(this).hasClass("disable")) 
+function handler () {
+  $(this).children("span").removeClass("noshow");
+  var that = this;
+  $.get($(this).text(), function (data) {
+    if ($(that).hasClass("disable")||$(that).children("span").hasClass("noshow"))
       return;
-    $(this).children("span").removeClass("noshow");
-    var that = this;
-    req.push($.get($(this).text(), function(data){
-        $(that).children("span").text(data);
-        $(that).removeClass('enable').addClass('disable');
-        var ifall = true
-        for (var i = 0; i < $(that).siblings().length; i++) {
-          if ($($(that).siblings()[i]).children("span").text() == "...") {
-            $($(that).siblings()[i]).removeClass('disable').addClass('enable');
-            ifall = false;
-          }
-        }
-        if (ifall) {
-          $("#info-bar").removeClass('disable').addClass('enable');
-          setTimeout("$('#info-bar').click()", 500);
-        }
-      })
-    );
-    $(this).unbind("click");
+    $(that).children("span").text(data);
+    $(that).removeClass('enable').addClass('disable');
+    var ifall = true;
+    for (var i = 0; i < $(that).siblings().length; i++) {
+      if ($($(that).siblings()[i]).children("span").text() == "...") {
+        $($(that).siblings()[i]).removeClass('disable').addClass('enable');
+        ifall = false;
+      }
+    }
+    if (ifall) 
+      bubbleHandler();
   });
-
-  $("#info-bar").click(function() {
-    if ($(this).hasClass("disable"))
-      return;
-    var sum = 0;
-    $.each($("span"), function(index, item){ 
-		  sum+= parseInt($.trim($(item).text()));
-    });
-
-    $(this).text(sum).removeClass('enable').addClass('disable');
-    $(this).click(function () {});
-  });
+  $(this).unbind("click");
 }
 
-function clear(req) {
-  while (req.length != 0) {
-    var temp = req.shift();
-    if (temp) temp.abort();
-  }
+function bubbleHandler () {
+  var sum = 0;
+  $.each($("span"), function(index, item){ 
+    sum+= parseInt($.trim($(item).text()));
+  });
+  $("#info-bar").removeClass('disable').addClass('enable');
+  setTimeout("$('#info-bar').text("+sum+").addClass('disable').removeClass('enable');", 500);
+}
+
+function clear () {
+  $('#info-bar').text("?");
+  $("#sequence").addClass("noshow");
   $("li").addClass("enable").removeClass("disable");
   $("span").text("...").addClass("noshow");
-  $("#info-bar").text("?").addClass("disable").removeClass("enable");
+  $("#info-bar").text("").addClass("disable").removeClass("enable");
+}
+
+function Init () {
+  $("li").click(handler);
+  $("li").click();
 }
